476c9005eb0a44bcec7aceb3879d1072c759d136
Kenta Komai <komaikenta@team-lab.com>
2015-03-11 17:20:37 +0900

    add gacha

--- app.rb
+++ app.rb
@@ -25,18 +25,28 @@ get '/lgtm' do
     ]
   )
 end
+get '/gacha' do
+  content_type :json
+  JSON.generate(
+    text:":star2:ガチャっと",
+    username:"パズドラガチャ",
+    attachments:[{
+      text:"",
+      image_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fmdfYkxmOHZ3UV84a0o4UjI0dkdVY3BjQXA3NWFHS0VsMHdYZ0Jqa1RjNjA/MONS_1578.PNG"
+    }],
+    icon_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fjlLQjZrM2lBQkRuTjR5VUU0UUpQSmx1UGdIZEl0eXdxQnFQczB0TmRwY0E/gacha.png"
+  )
+end
 
 post '/lgtm' do
   content_type :json
   JSON.generate(
-    text: "",
-    username: "LGTM",
-    attachments: [
-      {
-        text: "",
-        fallback: "lgtm bot",
-        image_url: "http://my-website.com/path/to/image.jpg"
-      }
-    ]
+    text:":star2:ガチャっと",
+    username:"パズドラガチャ",
+    attachments:[{
+      text:"",
+      image_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fmdfYkxmOHZ3UV84a0o4UjI0dkdVY3BjQXA3NWFHS0VsMHdYZ0Jqa1RjNjA/MONS_1578.PNG"
+    }],
+    icon_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fjlLQjZrM2lBQkRuTjR5VUU0UUpQSmx1UGdIZEl0eXdxQnFQczB0TmRwY0E/gacha.png"
   )
 end
