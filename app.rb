dc2afb9862c6bcf6caac193dbd85ca0f094418aa
Kenta Komai <komaikenta@team-lab.com>
2015-03-11 16:57:10 +0900

    edit post lgtm

--- app.rb
+++ app.rb
@@ -11,3 +11,12 @@ post '/' do
     text:"< HELLO"
   })
 end
+
+post '/lgtm' do
+  JSON.generate(
+    username: "LGTM",
+    attachments: [
+      {image_url: "http://my-website.com/path/to/image.jpg"}
+    ]
+  )
+end
