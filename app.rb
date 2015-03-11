76bb03f5b0ed7ebde81962297fb59b241b5d625b
Kenta Komai <komaikenta@team-lab.com>
2015-03-11 17:33:28 +0900

    add gacha

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ post '/' do
     text:"< HELLO"
   })
 end
+
 get '/lgtm' do
-  content_type :json
   JSON.generate(
     text: "",
     username: "LGTM",
@@ -25,17 +25,14 @@ get '/lgtm' do
     ]
   )
 end
+
 post '/lgtm' do
   JSON.generate(
     text:"LGTM",
     username:"LGTM",
     attachments:[{
-      text:"",
       image_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fmdfYkxmOHZ3UV84a0o4UjI0dkdVY3BjQXA3NWFHS0VsMHdYZ0Jqa1RjNjA/MONS_1578.PNG"
     }],
-    icon_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fjlLQjZrM2lBQkRuTjR5VUU0UUpQSmx1UGdIZEl0eXdxQnFQczB0TmRwY0E/gacha.png"
+    icon_url:"http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,h_540,w_720/tsjkqrdmdgfbuatbqklv.png"
   )
 end
-
-
-
