c625bdd18481b5dd2a2e3737e264d248203d38e8
Kenta Komai <komaikenta@team-lab.com>
2015-03-11 17:27:30 +0900

    add gacha

--- app.rb
+++ app.rb
@@ -26,32 +26,6 @@ get '/lgtm' do
   )
 end
 post '/lgtm' do
-  content_type :json
-  JSON.generate(
-    text:":star2:ガチャっと",
-    username:"パズドラガチャ",
-    attachments:[{
-      text:"",
-      image_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fmdfYkxmOHZ3UV84a0o4UjI0dkdVY3BjQXA3NWFHS0VsMHdYZ0Jqa1RjNjA/MONS_1578.PNG"
-    }],
-    icon_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fjlLQjZrM2lBQkRuTjR5VUU0UUpQSmx1UGdIZEl0eXdxQnFQczB0TmRwY0E/gacha.png"
-  )
-end
-get '/gacha' do
-  content_type :json
-  JSON.generate(
-    text:":star2:ガチャっと",
-    username:"パズドラガチャ",
-    attachments:[{
-      text:"",
-      image_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fmdfYkxmOHZ3UV84a0o4UjI0dkdVY3BjQXA3NWFHS0VsMHdYZ0Jqa1RjNjA/MONS_1578.PNG"
-    }],
-    icon_url:"https://www.googledrive.com/host/0B-ODBm7gVpU3fjlLQjZrM2lBQkRuTjR5VUU0UUpQSmx1UGdIZEl0eXdxQnFQczB0TmRwY0E/gacha.png"
-  )
-end
-
-post '/gacha' do
-  content_type :json
   JSON.generate(
     text:"LGTM",
     username:"LGTM",
